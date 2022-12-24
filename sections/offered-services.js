cc673dd0bb9007386d9a9d2643b707e754162f60
Csaba Boros <boros.csaba94@gmail.com>
2022-12-25 00:26:39 +0100

    Posts page

---
+++ sections/offered-services.js
@@ -0,0 +1,91 @@
+document.write(`
+<div class="section section-offered-services">
+    <div class="section-container">
+        <div class="section-title">Offered services</div>
+        <div class="section-services-container">
+            <div class="serivces-list">
+                <h2 class="serivces-list-title">Software development</h2>
+                <p>Hire me if you need someone who can hit the ground running! I am a super fast learner and with over 8 years of professional experience I can join you project and provide value from day 1!</p>
+                <br />
+                <h2 class="serivces-list-title">Software consulting</h2>
+                <p>You project isn't going the way you hoped? Let me take a look at your code base from a fresh perspective and help you finding the right direction for you project!</p>
+                <p>My current rate is <b>$125/h</b></p>
+            </div>
+            <div class="service-list-tech">
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg" />
+                    <div>Node.js</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+                    <div>C#</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dotnetcore/dotnetcore-original.svg" />
+                    <div>.NET Core</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />        
+                    <div>React</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+                    <div>Redux</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg" />
+                    <div>Selenium</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg" />
+                    <div>GraphQL</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />                
+                    <div>Docker</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-plain.svg" />
+                    <div>Gatsby</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" />
+                    <div>DevOps</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain.svg" />
+                    <div>SQL Server</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+                    <div>PostgreSQL</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" />
+                    <div>Angular</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" />
+                    <div>Bootstrap</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/d3js/d3js-original.svg" />
+                    <div>D3.js</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" />
+                    <div>Firebase</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <div>Javascript</div>
+                </div>
+                <div class="tech-chip">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xamarin/xamarin-original.svg" />
+                    <div>Xamarin</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+`);
