34203071fd51bdc1cdb913e824c6e0f7331f3782
Csaba Boros <boros.csaba94@gmail.com>
2023-03-06 16:22:52 +0100

    11ty config

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+        }
+    }
+}
