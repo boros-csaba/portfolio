078969197ebc5cf91e6baf40c08aebeafbfe6d9b
Csaba Boros <boros.csaba94@gmail.com>
2023-03-19 16:45:50 +0100

    Setup eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,72 @@
+const Image = require('@11ty/eleventy-img');
+
+const imageShortcode = async (
+  src,
+  alt,
+  className = undefined,
+  widths = [400, 800, 1280],
+  formats = ['webp', 'jpeg'],
+  sizes = '100vw'
+) => {
+  const imageMetadata = await Image(src, {
+    widths: [...widths, null],
+    formats: [...formats, null],
+    outputDir: '_site/assets/images',
+    urlPath: '/assets/images',
+  });
+
+  const sourceHtmlString = Object.values(imageMetadata)
+    .map((images) => {
+      const { sourceType } = images[0];
+      const sourceAttributes = stringifyAttributes({
+        type: sourceType,
+        srcset: images.map((image) => image.srcset).join(', '),
+        sizes,
+      });
+
+      return `<source ${sourceAttributes}>`;
+    })
+    .join('\n');
+
+  const getLargestImage = (format) => {
+    const images = imageMetadata[format];
+    return images[images.length - 1];
+  }
+
+  const largestUnoptimizedImg = getLargestImage(formats[0]);
+  const imgAttributes = stringifyAttributes({
+    src: largestUnoptimizedImg.url,
+    width: largestUnoptimizedImg.width,
+    height: largestUnoptimizedImg.height,
+    alt,
+    loading: 'lazy',
+    decoding: 'async',
+  });
+  const imgHtmlString = `<img ${imgAttributes}>`;
+
+  const pictureAttributes = stringifyAttributes({
+    class: className,
+  });
+  const picture = `<picture ${pictureAttributes}>
+    ${sourceHtmlString}
+    ${imgHtmlString}
+  </picture>`;
+
+  return outdent`${picture}`;
+};
+
+const stringifyAttributes = (attributeMap) => {
+    return Object.entries(attributeMap)
+      .map(([attribute, value]) => {
+        if (typeof value === 'undefined') return '';
+        return `${attribute}="${value}"`;
+      })
+      .join(' ');
+  };
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addShortcode('image', imageShortcode);
 
     return {
         dir: {
